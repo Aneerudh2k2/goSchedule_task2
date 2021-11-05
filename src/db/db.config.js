680a954211118765e3d86871f1e480879d20b5a9
Aneerudh2k2 <aneerudh@yahoo.com>
2021-11-05 17:56:43 +0530

    refactored

--- src/db/db.config.js
+++ src/db/db.config.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(
-  // "mongodb+srv://taskapp:taskapp@cluster0.ojjia.mongodb.net/rate_limit_api?retryWrites=true&w=majority",
-  process.env.DB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
